adbe90bda0ecad0a696371d826ba36fb13e20809
Marta G <marta.guszek@gmail.com>
2020-06-19 16:58:36 +0200

    Updates

---
+++ precache-manifest.8d9993fcd953199534ad8cc119382809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affeb9802f1064e67ae568801c14117d",
+    "url": "/deploy-exercise/index.html"
+  },
+  {
+    "revision": "a75f0fee96cd3529ebeb",
+    "url": "/deploy-exercise/static/css/main.f0f205b6.chunk.css"
+  },
+  {
+    "revision": "bcc96061b25f0edd2629",
+    "url": "/deploy-exercise/static/js/2.14eb5943.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-exercise/static/js/2.14eb5943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75f0fee96cd3529ebeb",
+    "url": "/deploy-exercise/static/js/main.a9297144.chunk.js"
+  },
+  {
+    "revision": "6de6bc9ac7fd903bc227",
+    "url": "/deploy-exercise/static/js/runtime-main.551f3692.js"
+  }
+]);
